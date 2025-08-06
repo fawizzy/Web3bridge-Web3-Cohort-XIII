bac571500aafa6f9d6ec532be08cf8aa7179ff8f
Fawaz Oduola <oduolafawaz@gmail.com>
2025-08-06 18:31:27 +0100

    task: deployed NFT on lisk sepolia

--- onsite-program/submissions/week-5/day-3/NFT-1/Fawaz-Oduola/ignition/modules/Lock.ts
+++ onsite-program/submissions/week-5/day-3/NFT-1/Fawaz-Oduola/ignition/modules/Lock.ts
@@ -10,7 +10,9 @@ const LockModule = buildModule("LockModule", (m) => {
   const unlockTime = m.getParameter("unlockTime", JAN_1ST_2030);
   const lockedAmount = m.getParameter("lockedAmount", ONE_GWEI);
 
-  const lock = m.contract("MyNFT");
+  const lock = m.contract("Lock", [unlockTime], {
+    value: lockedAmount,
+  });
 
   return { lock };
 });
